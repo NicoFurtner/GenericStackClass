64dd0af0c02b53b55d4f1a891067848a63c458e7
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-02-21 15:44:59 +0100

    push, pop, peek

--- src/GenericStackClass.java
+++ src/GenericStackClass.java
@@ -9,5 +9,42 @@ public class GenericStackClass<T> {
         this.zaehler = 0;
         this.stack = new Object[groeße];
     }
+    /**
+     * push method adds an element to the stack
+     * @param element
+     */
+    public void push(T element) throws StackFullException {
+        if(this.stack.length > zaehler + 1) {
+            throw new StackFullException();
+        }
+        stack[zaehler] = element;
+        zaehler++;
+    }
+    /**
+     * pop method removes the last added element
+     */
+    public void pop() throws StackEmptyException {
+        if(this.stack == null) {
+            throw new StackEmptyException();
+        }
+        else {
+            stack[zaehler - 1] = null;
+        }
+    }
+    public T peek() throws StackEmptyException {
+        if(this.stack == null) {
+            throw new StackEmptyException();
+        }
+        else {
+            return (T) stack[zaehler - 1];
+        }
+    }
 
+    public String list() {
+        String list = "";
+        for(int i = 0; i < stack.length; i++) {
+            list += stack[i] + ";";
+        }
+        return list;
+    }
 }
